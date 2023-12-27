96c553eba2ef06eca60edcc8f4e1fb9eab4583fa
Godzilla Developer <116243927+Gideon-Developer@users.noreply.github.com>
2023-12-27 17:35:02 +0200

    Add files via upload

---
+++ qb-input/fxmanifest.lua
@@ -0,0 +1,15 @@
+fx_version 'cerulean'
+game 'gta5'
+
+description 'QB-Input'
+version '1.2.0'
+
+shared_scripts {
+    '@ox_lib/init.lua'
+}
+
+client_scripts {
+    'client/*.lua'
+}
+
+lua54 'yes'
---
+++ qb-menu/fxmanifest.lua
@@ -0,0 +1,13 @@
+fx_version 'cerulean'
+game 'gta5'
+
+description 'QB-Menu'
+version '1.2.0'
+
+shared_scripts {
+    '@ox_lib/init.lua'
+}
+
+client_script 'client/main.lua'
+
+lua54 'yes'
